1b57f0a022fff02407e3339d55eb6425f3a11978
SagarikaTalukdar <sagarikatalukdar13@gmail.com>
2026-06-04 18:18:13 +0530

    Add files via upload

---
+++ SDET_Day21/LaunchDemoWebsite.java
@@ -0,0 +1,27 @@
+package SDET_Day21;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchDemoWebsite {
+public static void main(String[] args) throws InterruptedException {
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.saucedemo.com/");
+	driver.manage().window().maximize();
+	Thread.sleep(3000);
+	
+	driver.findElement(By.id("user-name")).sendKeys("standard_user");
+	Thread.sleep(1000);
+	
+	driver.findElement(By.id("password")).sendKeys("secret_sauce");
+	Thread.sleep(1000);
+	
+	driver.findElement(By.id("login-button")).click();
+	Thread.sleep(1000);
+	
+	System.out.println(driver.getTitle());
+	
+	driver.quit();
+}
+}
